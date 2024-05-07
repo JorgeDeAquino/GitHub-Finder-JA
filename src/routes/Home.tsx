6686aa8297b11ec27c852ef274874ef65bf96750
Jorge Alexandre <jorgeaamsantos@gmail.com>
2024-05-07 17:16:11 -0300

    add erro funcionando

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,25 +3,35 @@ import { UserProps } from "../types/user"
 import Search from "../components/Search"
 import { useState } from "react"
 import User from "../components/User"
+import Error from "../components/Error"
 
 
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null)
+  const [error, setError] = useState(false)
 
   const loadUser = async (userName: string) => {
+    setError(false)
+    setUser(null)
+
     const r = await fetch(`https://api.github.com/users/${userName}`)
 
     const data = await r.json()
+
+    if (r.status === 404) {
+      setError(true)
+      return
+    }
 
     console.log(data)
 
     const { avatar_url, login, location, followers, following } = data
 
     const userData: UserProps = {
-      avatar_url, 
-      login, 
-      location, 
-      followers, 
+      avatar_url,
+      login,
+      location,
+      followers,
       following,
     }
 
@@ -32,7 +42,8 @@ const Home = () => {
   return (
     <div>
       <Search loadUser={loadUser} />
-      {user && <User {...user}/>}
+      {user && <User {...user} />}
+      {error && <Error />}
     </div>
   )
 }
