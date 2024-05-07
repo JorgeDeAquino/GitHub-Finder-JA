f0029cb5888003bd516fd50ac1061a131413596f
Jorge Alexandre <jorgeaamsantos@gmail.com>
2024-05-07 17:08:41 -0300

    add if a localização

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,27 +1,29 @@
 import { MdLocationPin } from 'react-icons/md'
-import {UserProps} from '../types/user'
+import { UserProps } from '../types/user'
 import { Link } from 'react-router-dom'
 
 export default function User({ avatar_url, login, location, followers, following }: UserProps) {
-  return (
-    <div>
-        <img src={avatar_url} alt={login} />
-        <h2>{login}</h2>
-        <p>
-            <MdLocationPin />
-            <span>{location}</span>
-        </p>
+    return (
         <div>
+            <img src={avatar_url} alt={login} />
+            <h2>{login}</h2>
+            {location && (
+                <p>
+                    <MdLocationPin />
+                    <span>{location}</span>
+                </p>
+            )}
             <div>
-                <p>Seguindores:</p>
-                <p>{followers}</p>
+                <div>
+                    <p>Seguindores:</p>
+                    <p>{followers}</p>
+                </div>
+                <div>
+                    <p>Seguindo:</p>
+                    <p>{following}</p>
+                </div>
             </div>
-            <div>
-                <p>Seguindo:</p>
-                <p>{following}</p>
-            </div>
+            <Link to={`/repos/${login}`}>Ver melhores projetos</Link>
         </div>
-        <Link to={`/repos/${login}`}>Ver melhores projetos</Link>
-    </div>
-  )
+    )
 }
