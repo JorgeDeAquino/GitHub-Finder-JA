e421a5214716da9890d4803d65aea6b8024dd9f4
Jorge Alexandre <jorgeaamsantos@gmail.com>
2024-05-08 16:34:21 -0300

    add css ao user

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -2,9 +2,11 @@ import { MdLocationPin } from 'react-icons/md'
 import { UserProps } from '../types/user'
 import { Link } from 'react-router-dom'
 
+import classes from "./User.module.css"
+
 export default function User({ avatar_url, login, location, followers, following }: UserProps) {
     return (
-        <div>
+        <div className={classes.user}>
             <img src={avatar_url} alt={login} />
             <h2>{login}</h2>
             {location && (
@@ -13,14 +15,14 @@ export default function User({ avatar_url, login, location, followers, following
                     <span>{location}</span>
                 </p>
             )}
-            <div>
+            <div className={classes.stats}>
                 <div>
                     <p>Seguindores:</p>
-                    <p>{followers}</p>
+                    <p className={classes.number}>{followers}</p>
                 </div>
                 <div>
                     <p>Seguindo:</p>
-                    <p>{following}</p>
+                    <p className={classes.number}>{following}</p>
                 </div>
             </div>
             <Link to={`/repos/${login}`}>Ver melhores projetos</Link>
