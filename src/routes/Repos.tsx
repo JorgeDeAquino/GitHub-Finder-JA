780a3665445b31e46639ee88a7b59db6ebccc482
Jorge Alexandre <jorgeaamsantos@gmail.com>
2024-05-09 11:49:58 -0300

    testado requisição para a api com sucesso

--- src/routes/Repos.tsx
+++ src/routes/Repos.tsx
@@ -10,6 +10,26 @@ import BackBtn from "../components/BackBtn"
 export default function Repos() {
     const { username } = useParams()
 
+    const [repos, setRepos] = useState<RepoProps[] | [] | null>(null)
+    const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(() => {
+        
+        const loadRepos = async function(username: string) {
+            setIsLoading(true)
+
+            const r = await fetch(`https://api.github.com/users/${username}/repos`)
+
+            const data = await r.json()
+
+            setIsLoading(false)
+
+            console.log(data)
+        }
+        loadRepos(username)
+
+    }, [])
+
     return (
         <div>
             Repos {username}
